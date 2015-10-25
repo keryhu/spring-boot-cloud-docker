6e631bb7946c641346eced0fcc91f10bafe8a095
justindav1s <justin.davis@ba.com>
2015-10-25 15:39:09 +0000

    bug fixes

--- Basket/src/main/java/com/ib/booking/basket/BasketApplication.java
+++ Basket/src/main/java/com/ib/booking/basket/BasketApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 @EnableHystrix
+@EnableTurbine
 public class BasketApplication {
 
     public static void main(String[] args) {
--- Product/src/main/java/com/ib/booking/product/ProductApplication.java
+++ Product/src/main/java/com/ib/booking/product/ProductApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Configuration
 @EnableAutoConfiguration
 @EnableHystrix
+@EnableTurbine
 public class ProductApplication {
 
     public static void main(String[] args) {
