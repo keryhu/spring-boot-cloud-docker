e4a588f8457b046419a772d829e26a7ce5b6ac95
justindav1s <justin.davis@ba.com>
2015-10-31 16:24:03 +0000

    oauth2 updates

---
+++ authserver/src/main/java/com/ib/booking/auth/AuthServerApplication.java
@@ -0,0 +1,66 @@
+package com.ib.booking.auth;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+/**
+ * Created by justin on 31/10/2015.
+ */
+
+@SpringBootApplication
+@RestController
+@EnableResourceServer
+public class AuthServerApplication {
+
+    private Log log = LogFactory.getLog(AuthServerApplication.class);
+
+    @RequestMapping("/user")
+    public Principal user(Principal user) {
+        log.debug("/user called : "+user);
+        return user;
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServerApplication.class, args);
+    }
+
+    @Configuration
+    @EnableAuthorizationServer
+    protected static class OAuth2Config extends AuthorizationServerConfigurerAdapter {
+
+        @Autowired
+        private AuthenticationManager authenticationManager;
+
+        @Override
+        public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+            endpoints.authenticationManager(authenticationManager);
+        }
+
+        @Override
+        public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+            clients.inMemory()
+                    .withClient("acme")
+                    .secret("acmesecret")
+                    .authorizedGrantTypes("authorization_code", "refresh_token", "implicit", "password", "client_credentials")
+                    .scopes("webshop");
+        }
+    }
+
+}
+
+
+
