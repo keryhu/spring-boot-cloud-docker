bb8794b99ec4858873adc1cfacb2a8a12ecb14da
justindav1s <justin.davis@ba.com>
2015-11-05 10:44:09 +0000

    adding keycloak support

--- getkeycloakuserinfo.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-DOCKER_DAEMON_IP=`docker-machine ip default`
-ACCESS_TOKEN=`./getkeycloaktoken.sh`
-echo ACCESS TOKEN=${ACCESS_TOKEN}
-
-curl -v -H  "Authorization: Bearer ${ACCESS_TOKEN}" http://${DOCKER_DAEMON_IP}:8080/auth/realms/master/protocol/openid-connect/userinfo | jq
--- keycloak.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-docker run --name postgres -e POSTGRES_DATABASE=keycloak -e POSTGRES_USER=keycloak -e POSTGRES_PASSWORD=password -e POSTGRES_ROOT_PASSWORD=root_password -d postgres
-
-sleep 10
-
-docker run -p 8080:8080 --name keycloak --link postgres:postgres jboss/keycloak-postgres
-
