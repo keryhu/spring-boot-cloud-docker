e06e9c418b850ce59b38829fbd45855ed83cde18
justindav1s <justin.davis@ba.com>
2015-11-07 11:45:20 +0000

    adding keycloak proxy support

--- zuul-keycloak/src/main/java/com/ib/booking/zuul/ZuulApplication.java
+++ zuul-keycloak/src/main/java/com/ib/booking/zuul/ZuulApplication.java
@@ -4,19 +4,28 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
+//import org.springframework.cloud.security.oauth2.resource.EnableOAuth2Resource;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 
+@SpringBootApplication
 @Configuration
 @ComponentScan
 @EnableAutoConfiguration
 @Controller
 @EnableZuulProxy
-@EnableOAuth2Resource
-public class ZuulApplication {
+//@EnableOAuth2Resource
+public class ZuulApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+
+        return application.sources(ZuulApplication.class);
+
+    }
 
     public static void main(String[] args) {
 
