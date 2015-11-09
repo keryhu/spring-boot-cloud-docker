cb6168f820196ded10fd9a7ddcd95a2f0c0782f7
justindav1s <justin.davis@ba.com>
2015-11-09 11:58:58 +0000

    kafka integration

--- keycloak/keycloak.sh
+++ keycloak/keycloak.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-docker run --name postgres -e POSTGRES_DATABASE=keycloak -e POSTGRES_USER=keycloak -e POSTGRES_PASSWORD=password -e POSTGRES_ROOT_PASSWORD=root_password -d postgres
+docker run --name postgres -e POSTGRES_DATABASE=keycloak -e POSTGRES_USER=keycloak -e POSTGRES_PASSWORD=password -e POSTGRES_ROOT_PASSWORD=root_password -d justindav1s/postgres
 
 sleep 10
 
-docker run -d -p 9080:8080 --name keycloak --link postgres:postgres jboss/keycloak-postgres
+docker run -d -p 9080:8080 --name keycloak --link postgres:postgres justindav1s/keycloak-postgres
 
