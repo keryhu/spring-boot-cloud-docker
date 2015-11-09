c8b3594f0c286a55ea8e9a0f0c45d92117a55203
justindav1s <justin.davis@ba.com>
2015-11-09 18:04:55 +0000

    cleanup

--- test/token.sh
+++ test/token.sh
@@ -3,10 +3,9 @@
 DOCKER_DAEMON_IP=`docker-machine ip default`
 KC_IP=9080
 
-ACCESS_TOKEN=`curl -s iberia-services:568bb37a-db2c-4134-aac2-6deacb3e91a3@${DOCKER_DAEMON_IP}:${KC_IP}/auth/realms/master/protocol/openid-connect/token  \
- -d client_id=ios_app \
+ACCESS_TOKEN=`curl -s iberia.com:75ada485-dac9-40b6-be1d-b7c193d1e3c4@${DOCKER_DAEMON_IP}:${KC_IP}/auth/realms/Iberia_Commercial/protocol/openid-connect/token  \
  -d grant_type=password \
- -d username=justin \
+ -d username=ios_app \
  -d password=toffee | jq -r .access_token`
 
 echo ${ACCESS_TOKEN}
