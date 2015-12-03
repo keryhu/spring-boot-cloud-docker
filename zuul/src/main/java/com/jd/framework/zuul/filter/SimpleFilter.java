a84d524324ba153366f1185a5aedde1c3640cedf
justindav1s <justin.davis@ba.com>
2015-12-03 17:38:24 +0100

    zuul

--- zuul/src/main/java/com/jd/framework/zuul/filter/SimpleFilter.java
+++ zuul/src/main/java/com/jd/framework/zuul/filter/SimpleFilter.java
@@ -11,25 +11,32 @@ import org.springframework.stereotype.Component;
 @Component("SimpleFilter")
 public class SimpleFilter extends ZuulFilter {
 
-    Logger log = LoggerFactory.getLogger(SimpleFilter.class);
+    private static Logger log = LoggerFactory.getLogger(SimpleFilter.class);
 
     @Override
     public String filterType() {
-        return "pre";
+        String type = "pre";
+        log.debug("Filter Type : "+type);
+        return type;
     }
 
     @Override
     public int filterOrder() {
-        return 1;
+        int order = 1;
+        log.debug("Filter Order : "+order);
+        return order;
     }
 
     @Override
     public boolean shouldFilter() {
+        boolean filter = true;
+        log.debug("Filter  : "+filter);
         return true;
     }
 
     @Override
     public Object run() {
+        log.debug("run !");
         return null;
     }
 }
